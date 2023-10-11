8fa814272e49d1ca32b096b62752e0c1105aee36
Ayaaaa02 <139942884+Ayaaaa02@users.noreply.github.com>
2023-10-11 12:39:39 +0000

    新規登録画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+  
+  
+ # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  namespace :public do
+   root 'homes#top'
+   get "/about" => "homes#about"
+   get "/customers/mypage" => "customers#show"
+   get "/customers/information/edit" => "customers#edit"
+   patch "/customers" => "customers#update"
+  end
+
+
+
+# 店舗用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
