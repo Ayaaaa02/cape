3c124720526e1b7b6812e2325e2befda5e2625c8
Ayaaaa02 <139942884+Ayaaaa02@users.noreply.github.com>
2023-10-12 04:34:17 +0000

    登録編集画面作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,11 +1,17 @@
 class Public::CustomersController < ApplicationController
     
     def show
+     @customer = current_customer
     end
     
     def edit
+      @customer = current_customer
+      @customers = Customer.all
     end
     
     def update
+     @customer = current_customer
+     @customer.update(customer_params)
+     redirect_to public_customers_mypage_path
     end
 end
