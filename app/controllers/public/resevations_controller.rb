8db14326ece565d8f43c634bd1288133028157e7
Ayaaaa02 <139942884+Ayaaaa02@users.noreply.github.com>
2023-10-17 04:14:15 +0000

    店舗ログイン作成

--- app/controllers/public/resevations_controller.rb
+++ app/controllers/public/resevations_controller.rb
@@ -1,10 +1,25 @@
 class Public::ResevationsController < ApplicationController
   def new
+    @resevation = Resevation.new
   end
 
   def confirm
   end
 
   def thanks
+  end
+  
+  def create
+    @resevation = Resevation.new(resevation_params)
+  　if @resevation.save
+    　redirect_to public_resevations_new_path(@resevation)
+  　else 
+    　render :new
+  end
+  
+  
+   private
+  def resevation_params
+    params.require(:resevation).permit(:time, :people, :payment_method, :date)
   end
 end
